dd924899bc9262112677a548e39b71134fe2de10
dptran <darren-tran@hotmail.com>
2021-12-03 16:12:27 -0800

    latest

--- static/js/main.js
+++ static/js/main.js
@@ -13,19 +13,18 @@ form.addEventListener('click', (event) => {
 const weather_data = () => {
     let city = document.querySelector('#city').value
     let api_key = 'e8d5b83f2b655c2f76ab2da382b63265'
-    axios.get(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`).then(response => {
-        data = response.data.list[0]
-        if (data.length > 0) {
-            var temp = "";
-            data.forEach((itemData) => {
-                temp += "<tr>";
-                temp += "<td>" + itemData.weather[0].main + "and" + itemData.weather[1].main + "</td>";
-                temp += "<td>" + itemData.main.temp_max + "</td>";
-                temp += "<td>" + itemData.main.temp_min + "</td>"
-                temp += "<td>" + itemData.main.humidity + "</td></tr>";
-            });
-            document.getElementById('data').innerHTML = temp;
-        }
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${api_key}`).then(response => {
+        data = response.data.main
+        weather = response.data.weather
+
+        var temp = "";
+        temp += "<tr>";
+        temp += "<td>" + weather[0]['main'] + "</td>";
+        temp += "<td>" + data['temp_max'] + "</td>";
+        temp += "<td>" + data['temp_min'] + "</td>"
+        temp += "<td>" + data['humidity'] + "</td></tr>";
+        document.getElementById('data').innerHTML = temp;
+      
     });
 }
 
